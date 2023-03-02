6cd4ed99e76db5b8fda0438a582d886faf1d071f
thebao <lethebao2507@gmail.com>
2023-03-02 11:17:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+
+function App() {
+    return (
+        <>
+            <Hello/>
+            <div></div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+    return <div className="text-sky-400 text-3xl">Hello</div>;
+};
+
+export default Hello;
